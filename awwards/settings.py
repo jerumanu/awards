b417e0f355d8809e135c96531152015d2038bcf9
jerumanu <emmanueljeru200@email.com>
2021-01-24 12:25:00 +0300

    urls

--- awwards/settings.py
+++ awwards/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project'
-    'rest_framework'
-    'django_countries'
+    'project',
+    'rest_framework',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
