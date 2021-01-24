4b142253736f5da0ee41737f659deb09ced39005
jerumanu <emmanueljeru200@email.com>
2021-01-24 11:57:22 +0300

    add importation to views

--- project/views.py
+++ project/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Profile, Projects
+from .serializer import  ProfileSerializer,ProjectsSerializer
 # Create your views here.
