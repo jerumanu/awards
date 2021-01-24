49a7d36198beb9037b2f61e59f2fd0081eb70c3e
jerumanu <emmanueljeru200@email.com>
2021-01-24 11:53:25 +0300

    add profile serializer

--- project/serializer.py
+++ project/serializer.py
@@ -6,3 +6,7 @@ class ProjectsSerializer(serializers.ModelSerializer):
         model = Projects
         fields = ('id', 'project_title', 'project_image', 'project_description', 'pub_date', 'author', 'author_profile', 'link','country')
         
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'project_title', 'project_image', 'project_description', 'pub_date', 'author', 'author_profile', 'link','country')        
