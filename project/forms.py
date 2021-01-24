95942a7b9fc8f67fbb889887a7c2ca794f3a9b75
jerumanu <emmanueljeru200@email.com>
2021-01-24 11:38:46 +0300

    created forms.py

---
+++ project/forms.py
@@ -0,0 +1,12 @@
+from django  import forms
+from .models import *
+
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['author', 'pub_date', 'author_profile']
+        widgets = {'project_description': forms.Textarea(attrs={'rows':4, 'cols':10,}),
+        }
+
+        
--- project/models.py
+++ project/models.py
@@ -5,6 +5,24 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = CloudinaryField('profile_pics/', blank=True)
+
+    def save_profile(self):
+        self.save()                   
+
+    def delete_profile(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.bio
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'  
 class Projects(models.Model):
     project_title = models.CharField(max_length=255)
     project_image = CloudinaryField('images')
