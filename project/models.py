70a00c3125c08e165d05648a43da54f6ca09f289
jerumanu <emmanueljeru200@email.com>
2021-01-24 10:30:34 +0300

    importation

--- project/models.py
+++ project/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+import datetime as dt
+from django_countries.fields import CountryField
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Projects(models.Model):
