70dd6caed88b747f05d40c90cea9749996199449
jerumanu <emmanueljeru200@email.com>
2021-01-24 10:28:13 +0300

    add class

--- project/models.py
+++ project/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_image = CloudinaryField('images')
+    project_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    link = models.URLField()
+    country = CountryField(blank_label='(select country)', default='Kenya')
+        
+    def save_project(self):
+        self.save()
+    
+    def delete_project(self):
+        self.delete()
+        
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(project_title__icontains=search_term)
+        return projects
